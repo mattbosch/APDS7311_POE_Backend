50360efdc44ad7fc9110cb211875a4bc5dcfd8dd
mattbosch <mattbosch16@gmail.com>
2023-10-11 13:57:31 +0200

    Added lastName to user model.

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const Joi = require('joi');
 const userschema = mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
-    firstName: { type: String, required: true }
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
 });
 
 const User = mongoose.model('User', userschema);
@@ -15,7 +16,8 @@ function validateUser(user) {
     const schema = Joi.object({
         username: Joi.string().min(6).max(35).required(),
         password: Joi.string().min(6).max(35).required(),
-        firstName: Joi.string().min(3).max(55).required()
+        firstName: Joi.string().max(55).required(),
+        lastName: Joi.string().max(55).required()
     });
     return schema.validate(user);
 }
